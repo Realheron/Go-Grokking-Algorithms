230ec3cbd38e1dce0940d57a7b54ade3bddd1722
Heron <theheron666@gmail.com>
2025-04-19 14:38:18 +0330

    Update 4.3.go

--- Season 4 /4.3.go
+++ Season 4 /4.3.go
@@ -1,1 +1,22 @@
 
+package main
+
+import (
+	"fmt"
+)
+
+func findMaxRecursive(lst []int) int {
+	if len(lst) == 1 {
+		return lst[0]
+	}
+	maxRest := findMaxRecursive(lst[1:])
+	if lst[0] > maxRest {
+		return lst[0]
+	}
+	return maxRest
+}
+
+func main() {
+	numbers := []int{4, 10, 2, 33, 5}
+	fmt.Println(findMaxRecursive(numbers))
+}
